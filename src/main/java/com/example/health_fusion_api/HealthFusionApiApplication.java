d0dc962880b0d6dcb674a0f219de4e980dcd5373
Hari-Nikesh-R <hari.nikesh.r.cce@gmail.com>
2024-06-30 17:20:40 +0530

    initial commit

---
+++ src/main/java/com/example/health_fusion_api/HealthFusionApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.health_fusion_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthFusionApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthFusionApiApplication.class, args);
+	}
+
+}
